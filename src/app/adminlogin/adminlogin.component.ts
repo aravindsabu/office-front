0eab116d6eba605c09c2ff35aa6582eed58a78ff
aravindsabu <aravindsabu2001@gmail.com>
2022-10-28 15:37:01 +0530

    ok

---
+++ src/app/adminlogin/adminlogin.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-adminlogin',
+  templateUrl: './adminlogin.component.html',
+  styleUrls: ['./adminlogin.component.css']
+})
+export class AdminloginComponent implements OnInit {
+
+  constructor(private myrouter:Router) { }
+  username=""
+  password=""
+  readValues=()=>{
+    let data={
+      "username":this.username,
+      "password":this.password
+    }
+    if (this.username=="admin" && this.password=="123456"){
+      this.myrouter.navigate(["/ok"])
+    }
+    else{
+      alert("invalid credential")
+    }
+    console.log(data)
+    alert("success")
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { EmployeeloginComponent } from './employeelogin/employeelogin.component';
+import { SecurityloginComponent } from './securitylogin/securitylogin.component';
+import { FormsModule } from '@angular/forms';
+
 const appRoutes:Routes=[
   {
-    
+    path:"",component:AdminloginComponent
+  },
+  {
+    path:"ok",component:EmployeeloginComponent
+  },
+  {
+    path:"on",component:SecurityloginComponent
   }
+
 ]
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdminloginComponent,
+    EmployeeloginComponent,
+    SecurityloginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
