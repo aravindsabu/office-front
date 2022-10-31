87a0e5eda24589024f86bff78048ed631ef3323c
aravindsabu <aravindsabu2001@gmail.com>
2022-10-31 10:43:51 +0530

    ok

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-employee',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) {
+    this.fetchData()
+   }
+   fetchData=()=>{
+    this.myapi.viewEmploye().subscribe(
+      (data)=>{
+        this.root=data
+      }
+    )
+   }
+   deleteEmployee=(employee_id:any)=>{
+    let data={
+      "employee_id":employee_id
+
+    }
+    this.myapi.deleteEmployee(data).subscribe(
+      (resp)=>{
+        alert("delete")
+      }
+    )
+    this.fetchData()
+   }
+   root:any=[]
+
 
   ngOnInit(): void {
   }
